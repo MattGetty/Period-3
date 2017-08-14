71d220360c1f2e61c048758f1d2cada1eac08552
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2017-08-14 10:41:55 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //JAVASCRIPT BOI
 function sayHIFOOL(){//function called sayHIFOOL
-  var userName = prompt("whats ur name boi");//variable that asks user whats their name and saves answer
-  window.alert(userName + " " + "Hello");//window alert that pops up that variable
+  var userName = prompt("whats ur first name dum kid");//variable that asks user whats their name and saves answer
+  var nickName = prompt("Hey " + userName + "," + " do you have a nickname?");
+  var lastName = prompt("Whats ur last name dum kid?");
+  
+  window.alert( "Hello " + userName + " " + lastName + " Also known as " + nickName);//window alert that pops up that variable
 }
