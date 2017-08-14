1f9a5e83fd23bb59bfc8b93798827e1a60ac7948
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2017-08-14 10:33:36 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-qeiughauoehguiawehgopuwaehrpguoihearipug
+//JAVASCRIPT BOI
+function sayHIFOOL(){//function called sayHIFOOL
+  var userName = prompt("whats ur name boi");//variable that asks user whats their name and saves answer
+  window.alert(userName + " " + "Hello");//window alert that pops up that variable
+}
