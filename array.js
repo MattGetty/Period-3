08059bc6180260371c0de64cdd209bc020e1e64d
Matt Getty <30877976+MattGetty@users.noreply.github.com>
2017-08-16 10:20:33 -1000

    Add files via upload

---
+++ array.js
@@ -0,0 +1,6 @@
+var juniors = ["Reimon", "Hana"];
+var seniors = ["Samiyah", "Justin", "DYSON"];
+//show total number of juniors
+//show total number of seniors
+//1 popup window
+window.alert("There are " + juniors.length + " juniors " + "and " + seniors.length + " seniors.")
